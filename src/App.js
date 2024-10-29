b50038204c2eeafec214bb3b8a5429b981dd3743
subha1101 <subha@entrans.ai>
2024-10-29 13:05:29 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -15,31 +15,3 @@ function App() {
 }
 
 export default App;
-<<<<<<< HEAD
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-//     </div>
-//   );
-// }
-=======
->>>>>>> 2b05f18eaa6f7657362f0625f5e1cd8415e5622e
